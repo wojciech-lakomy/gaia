d2b011a19fe61ef9404abe3ac86e61cc7b9fc60d
Wojciech Lakomy <wojciech.lakomy@rec-global.com>
2014-12-05 13:14:40 +0100

    Add some unit tests

--- apps/privacy-panel/test/unit/ala/define_custom_location_test.js
+++ apps/privacy-panel/test/unit/ala/define_custom_location_test.js
@@ -1,35 +1,57 @@
 'use strict';
 
-var htmlHelper;
-//var realMozSettings;
+var htmlHelper, realL10n;
 
 suite('ALA CustomLocation', function() {
 
   suiteSetup(function(done) {
-    require(['html_helper'], function(html) {
+    require(['html_helper', 'mocks/mock_l10n'], function(html, MockL10n) {
       htmlHelper = html;
+
+      realL10n = navigator.mozL10n;
+      navigator.mozL10n = MockL10n;
+
       done();
     });
   });
 
   setup(function(done) {
     require(['ala/define_custom_location'], ALADefineCustomLocation => {
-      var section, test;
 
       this.subject = ALADefineCustomLocation;
-      this.template = htmlHelper.get
-      ('../../templates/ala/custom.html');
+      this.template = htmlHelper.get('../../templates/ala/custom.html');
 
-      test = document.getElementById('test');
-      section = document.createElement('section');
-      section.id = 'ala-custom';
-      section.innerHTML = this.template;
+      this.element = document.getElementById('test');
+      this.element.appendChild(this.template);
 
-      test.appendChild(section);
+      this.subject.init();
+
+      // prepare some fake localizations
+      this.subject.regionsAndCities = {
+        'africa': {
+          'dakar': { 'lat': 14.76, 'lon': -17.37, 'city': 'Dakar' },
+          'johannesburg': { 'lat': -26.2, 'lon': 28.05, 'city': 'Johannesburg' }
+        },
+        'america': {
+          'chicago': { 'lat': 41.88, 'lon': -87.63, 'city': 'Chicago' },
+          'los-angeles': { 'lat': 34.05, 'lon': -118.24, 'city': 'LA' }
+        },
+        'europe': {
+          'rome': { 'lat': 41.87, 'lon': 12.48, 'city': 'Rome' },
+          'warsaw': { 'lat': 52.23, 'lon': 21.01, 'city': 'Warsaw' }
+        }
+      };
 
       done();
     });
   });
+
+  suiteTeardown(function() {
+    navigator.mozL10n = realL10n;
+  });
+
+
+  // validation tests //////////////////////////////////////////////////////////
 
   test('should validate true coords: 0, 0', function() {
     this.subject.config = {
@@ -157,5 +179,101 @@ suite('ALA CustomLocation', function() {
       longitude: ''
     };
     assert.isFalse(this.subject.validate());
+  });
+
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  test('should display default values', function() {
+    this.subject.onBeforeShow({
+      detail: {
+        getDCLData: function() {
+          return {}; // no DCL data
+        },
+        saveDCLData: function(){},
+        goBackFromDCL: function(){}
+      }
+    });
+
+    // check config values
+    assert.equal(this.subject.config.type, 'rc');
+    assert.equal(this.subject.config.region, 'africa');
+    assert.equal(this.subject.config.city, 'dakar');
+    assert.equal(this.subject.config.longitude, -17.37);
+    assert.equal(this.subject.config.latitude, 14.76);
+
+    // check html values
+    assert.equal(this.element.querySelector('.dcl-type-rc').checked, true);
+    assert.equal(this.element.querySelector('.dcl-region').value, 'africa');
+    assert.equal(this.element.querySelector('.dcl-city').value, 'dakar');
+    assert.equal(this.element.querySelector('.dcl-type-gps').checked, false);
+    assert.equal(this.element.querySelector('.dcl-longitude').value, -17.37);
+    assert.equal(this.element.querySelector('.dcl-latitude').value, 14.76);
+  });
+
+  test('should display region/city values', function() {
+    this.subject.onBeforeShow({
+      detail: {
+        getDCLData: function() {
+          return {
+            type: 'rc',
+            region: 'america',
+            city: 'los-angeles',
+            longitude: -118.24,
+            latitude: 34.05
+          };
+        },
+        saveDCLData: function(){},
+        goBackFromDCL: function(){}
+      }
+    });
+
+    // check config values
+    assert.equal(this.subject.config.type, 'rc');
+    assert.equal(this.subject.config.region, 'america');
+    assert.equal(this.subject.config.city, 'los-angeles');
+    assert.equal(this.subject.config.longitude, -118.24);
+    assert.equal(this.subject.config.latitude, 34.05);
+
+    // check html values
+    assert.equal(this.element.querySelector('.dcl-type-rc').checked, true);
+    assert.equal(this.element.querySelector('.dcl-region').value, 'america');
+    assert.equal(this.element.querySelector('.dcl-city').value, 'los-angeles');
+    assert.equal(this.element.querySelector('.dcl-type-gps').checked, false);
+    assert.equal(this.element.querySelector('.dcl-longitude').value, -118.24);
+    assert.equal(this.element.querySelector('.dcl-latitude').value, 34.05);
+  });
+
+  test('should display gps values', function() {
+    this.subject.onBeforeShow({
+      detail: {
+        getDCLData: function() {
+          return {
+            type: 'gps',
+            region: 'america',
+            city: 'los-angeles',
+            longitude: -118.24,
+            latitude: 34.05
+          };
+        },
+        saveDCLData: function(){},
+        goBackFromDCL: function(){}
+      }
+    });
+
+    // check config values
+    assert.equal(this.subject.config.type, 'gps');
+    assert.equal(this.subject.config.region, 'america');
+    assert.equal(this.subject.config.city, 'los-angeles');
+    assert.equal(this.subject.config.longitude, -118.24);
+    assert.equal(this.subject.config.latitude, 34.05);
+
+    // check html values
+    assert.equal(this.element.querySelector('.dcl-type-rc').checked, false);
+    assert.equal(this.element.querySelector('.dcl-region').value, 'america');
+    assert.equal(this.element.querySelector('.dcl-city').value, 'los-angeles');
+    assert.equal(this.element.querySelector('.dcl-type-gps').checked, true);
+    assert.equal(this.element.querySelector('.dcl-longitude').value, -118.24);
+    assert.equal(this.element.querySelector('.dcl-latitude').value, 34.05);
   });
 });
